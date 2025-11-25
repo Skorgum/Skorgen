4e7a6b3133b3cec17c623ae5682fa8864b803936
Skorgum <skorgum@gmail.com>
2025-11-25 12:20:38 -0500

    Test LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
@@ -15,3 +15,24 @@ class TestHTMLNode(unittest.TestCase):
         node_with_props = HTMLNode(props={})
         html_string = node_with_props.props_to_html()
         self.assertEqual(html_string, "")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, skorgum!")
+        self.assertEqual(node.to_html(), "<p>Hello, skorgum!</p>")
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Hello, skorgum!")
+        self.assertEqual(node.to_html(), "<a>Hello, skorgum!</a>")
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello, skorgum!")
+        self.assertEqual(node.to_html(), "<b>Hello, skorgum!</b>")
+    def test_leaf_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    def test_no_tag(self):
+        node = LeafNode(None, "I forgot my tag!")
+        self.assertEqual(node.to_html(), "I forgot my tag!")
+    def test_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
