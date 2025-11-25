d4763520ebb197c3fc117d172e243647b4f0712d
Skorgum <skorgum@gmail.com>
2025-11-25 10:08:24 -0500

    Add HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,21 @@
 class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
-        pass
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        
+        props_parts = []
+        for prop_name, prop_value in self.props.items():
+            props_parts.append(f' {prop_name}="{prop_value}"')
+        return "".join(props_parts)
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
