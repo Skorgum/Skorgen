e4258898bec56a2d0c2266bc43bc4a6c78da6dfe
Skorgum <skorgum@gmail.com>
2025-11-27 17:15:34 -0500

    Add and test text to textnode

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -149,6 +149,56 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" and a ", TextType.TEXT),
                 TextNode("link", TextType.LINK, "https://www.google.com"),
             ],
+            new_nodes,
+        )
+
+    def test_text_to_textnode(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            new_nodes,
+        )
+    
+    def test_no_markdown(self):
+        text = "This is text with no markdown"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is text with no markdown", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+    
+    def test_multiple_bold(self):
+        text = "This is **bold** text because I **love bold**"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" text because I ", TextType.TEXT),
+                TextNode("love bold", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+
+    def text_no_text(self):
+        text = ""
+        new_nodes = text_to_textnodes(text)
+        self.assertEqual(
+            [],
             new_nodes,
         )
 
