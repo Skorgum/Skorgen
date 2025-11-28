3473bd461ddcb855393d04e8492afe313cbb09cd
Skorgum <skorgum@gmail.com>
2025-11-28 16:17:29 -0500

    Add and test markdown to html

---
+++ src/test_markdown_to_html.py
@@ -0,0 +1,85 @@
+import unittest
+from main import markdown_to_html_node
+
+class TestMarkdownToHtml(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+    )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+    )
+        
+    def test_quoteblock(self):
+        md = """
+> This is a **bold** quote
+> This is an _italic_ quote
+> This is a third quote
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a <b>bold</b> quote This is an <i>italic</i> quote This is a third quote</blockquote></div>"
+        )
+
+    def test_headingblock(self):
+        md = """
+### This is heading # 3
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h3>This is heading # 3</h3></div>"
+        )
+
+    def test_ulistblock(self):
+        md = """
+- first
+- second
+- third
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>first</li><li>second</li><li>third</li></ul></div>"
+        )
+
+    def test_olistblock(self):
+        md = """
+1. first
+2. second
+3. third
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>first</li><li>second</li><li>third</li></ol></div>"
+        )
