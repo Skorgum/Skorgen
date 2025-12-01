563524e596d63a6f8afcdf7b353f08e8bf65a800
Skorgum <skorgum@gmail.com>
2025-11-30 19:55:42 -0500

    Correct pathing issues for deeper pages

--- src/main.py
+++ src/main.py
@@ -10,7 +10,19 @@ def main():
         shutil.rmtree(public_dir)
 
     copy_static("static", public_dir)
-    generate_page("content/index.md", "template.html", f"{public_dir}/index.html")
+    # generate_page("content/index.md", "template.html", f"{public_dir}/index.html")
+    for root, dirs, files in os.walk("content"):
+        for filename in files:
+            if not filename.endswith(".md"):
+                continue
+            from_path = os.path.join(root, filename)
+
+            relative_path = from_path[len("content/"):]
+
+            dest_path = os.path.join(public_dir, relative_path)
+            dest_path = dest_path.replace(".md", ".html")
+
+            generate_page(from_path, "template.html", dest_path)
 
 if __name__ == "__main__":
     main()
